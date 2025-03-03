af6a71f65cfb261ded0df9b18b01f554af04131f
Simon Høxbro Hansen <simon.hansen@me.com>
2025-03-03 11:00:06 +0100

    Make env variable

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,8 @@
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
+set CARGO_BUILD_PROFILE=release-windows
 
-cargo install --no-track --locked --bins --root %LIBRARY_PREFIX% --profile release-windows yazi-fm yazi-cli
+cargo install --no-track --locked --bins --root %LIBRARY_PREFIX% yazi-fm yazi-cli
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 cargo-bundle-licenses --format yaml --output %SRC_DIR%/THIRDPARTY.yml
 if %errorlevel% NEQ 0 exit /b %errorlevel%
