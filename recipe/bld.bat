47cdb50c78279280c4c98ff7fc1b3ca464bd3a35
Simon Høxbro Hansen <simon.hansen@me.com>
2025-04-10 10:05:04 +0200

    add --path .

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,7 @@
 set CARGO_PROFILE_RELEASE_STRIP=symbols
 set CARGO_PROFILE_RELEASE_LTO=fat
 
-cargo install --no-track --profile release-windows --locked --bins --root %LIBRARY_PREFIX% yazi-fm 
-if %errorlevel% NEQ 0 exit /b %errorlevel%
-cargo install --no-track --locked --bins --root %LIBRARY_PREFIX% yazi-cli 
+cargo install --path . --no-track --profile release-windows --locked --bins --root %LIBRARY_PREFIX% yazi-fm yazi-cli 
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 cargo-bundle-licenses --format yaml --output %SRC_DIR%/THIRDPARTY.yml
 if %errorlevel% NEQ 0 exit /b %errorlevel%
