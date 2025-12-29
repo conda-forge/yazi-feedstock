55675478f3ef01e98e6995655e894afe4e97eb9f
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-29 09:53:57 +0100

    just add yazi-build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,5 +3,9 @@ set CARGO_PROFILE_RELEASE_LTO=fat
 
 cargo install --path %SRC_DIR%/yazi-build --no-track --locked --bins --root %LIBRARY_PREFIX% --profile release-windows yazi-build
 if %errorlevel% NEQ 0 exit /b %errorlevel%
+cargo install --path %SRC_DIR%/yazi-fm --no-track --locked --bins --root %LIBRARY_PREFIX% --profile release-windows yazi-fm
+if %errorlevel% NEQ 0 exit /b %errorlevel%
+cargo install --path %SRC_DIR%/yazi-cli --no-track --locked --bins --root %LIBRARY_PREFIX% --profile release-windows yazi-cli
+if %errorlevel% NEQ 0 exit /b %errorlevel%
 cargo-bundle-licenses --format yaml --output %SRC_DIR%/THIRDPARTY.yml
 if %errorlevel% NEQ 0 exit /b %errorlevel%
--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,5 @@ set -euxo pipefail
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
-cargo install --no-track --locked --root "${PREFIX}" yazi-build
+cargo install --no-track --locked --root "${PREFIX}" yazi-build yazi-fm yazi-cli
 cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
