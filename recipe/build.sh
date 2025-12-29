09e6f354e02f900f77a7bd104e045fabfb55239b
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-29 11:32:13 +0100

    seperate yazi-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,5 +4,6 @@ set -euxo pipefail
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
-cargo install --no-track --locked --root "${PREFIX}" yazi-build yazi-fm yazi-cli
+cargo install --no-track --locked yazi-build
+cargo install --no-track --locked --root "${PREFIX}" yazi-fm yazi-cli
 cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
