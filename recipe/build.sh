633778b67d1ed865d4ff91407131db9c456a06cc
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-29 12:30:23 +0100

    mimic windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -euxo pipefail
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
-cargo install --no-track --locked yazi-build
-cargo install --no-track --locked --root "${PREFIX}" yazi-fm yazi-cli
+cargo install --path "${SRC_DIR}/yazi-build" --no-track --locked --bins yazi-build
+cargo install --path "${SRC_DIR}/yazi-fm" --no-track --locked --bins --root "${PREFIX}" yazi-fm
+cargo install --path "${SRC_DIR}/yazi-cli" --no-track --locked --bins --root "${PREFIX}" yazi-cli
 cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
