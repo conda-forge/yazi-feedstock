7627bb77d585af1126bdecbd7c707a21a6181ade
Simon Høxbro Hansen <hoxbro@protonmail.com>
2025-12-29 09:11:41 +0100

    try using yazi-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,5 @@ set -euxo pipefail
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 export CARGO_PROFILE_RELEASE_LTO=fat
 
-cargo install --no-track --locked --root "${PREFIX}" yazi-fm yazi-cli
+cargo install --no-track --locked --root "${PREFIX}" yazi-build
 cargo-bundle-licenses --format yaml --output "${SRC_DIR}/THIRDPARTY.yml"
-
